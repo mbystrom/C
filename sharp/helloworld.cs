90c24ef051277f100b6697ce5cf04bb3306885df
penelope <njwbys@gmail.com>
2018-07-24 17:46:31 -0700

    Initial commit

---
+++ sharp/helloworld.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HelloWorld
+{
+	class helloworld {
+		static void Main (string[] args) {
+			Console.WriteLine("Hello World!");
+			Console.ReadKey();
+		}
+	}
+}
