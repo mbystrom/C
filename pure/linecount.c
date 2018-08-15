8cfde3b284fd1fb8dddcc7a1c2fd4f0daa39f265
Noah Bystrom <noah.bystrom@calbt.com>
2018-08-15 11:02:13 -0700

    Add file counting lines and characters in a file
    filename defined in macro

---
+++ pure/linecount.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define FILENAME "./factorial.c"
+
+int main ()
+{
+    FILE *file;
+    file = fopen(FILENAME, "r");
+    long charCount = 0;
+    long lineCount = 0;
+    
+    int c;
+    while ((c = fgetc(file)) != EOF) {
+        ++charCount;
+        if (c == '\n') ++lineCount;
+    }
+    printf("your file is %ld lines\nit is %ld characters", lineCount, charCount);
+} 
