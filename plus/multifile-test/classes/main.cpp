8e639efecba9386c0727a75e852fda06b71023fe
naiomi <njwbys@gmail.com>
2018-08-16 21:19:33 +0000

    Add a dynamic list class (don't actually use if please)
    This was just for testing purposes, and so that I can say I did it

--- plus/multifile-test/classes/main.cpp
+++ plus/multifile-test/classes/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "classdecl.class"
+#include "list.class"
 
 using namespace std;
 
@@ -14,4 +15,23 @@ int main ()
     cout << "obj1 num is " << obj1.getNum() << endl;
     cout << "obj2 num is " << obj2.getNum() << endl;
 
+    List ls = List();
+    ls.append(1);
+    ls.append(2);
+    ls.append(3);
+    ls.append(4);
+    ls.append(5);
+    ls.append(6);
+    ls.append(7);
+    ls.append(8);
+    ls.append(9);
+
+    cout << "ls is " << ls.length() << " members long" << endl;
+    cout << "ls[4] is " << ls.retrieve(4) << endl;
+
+    cout << "popping last item! it is: " << ls.pop() << endl;
+    cout << "popping third item! it is: " << ls.pop(2) << endl;
+
+    cout << "deleting list!" << endl;
+    ls.~List();
 }
